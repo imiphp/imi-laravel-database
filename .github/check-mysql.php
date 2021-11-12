8472e75ed0f861481b043e2fd05c6a88f4381ff2
Yurun <admin@yurunsoft.com>
2021-11-12 17:25:54 +0800

    等待mysql启动完毕

---
+++ .github/check-mysql.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+for ($i = 0; $i < 30; ++$i)
+{
+    try
+    {
+        new PDO('mysql:'
+        . 'host=' . (getenv('MYSQL_SERVER_HOST') ?: '127.0.0.1')
+        . ';port=' . (getenv('MYSQL_SERVER_PORT') ?: 3306)
+        . ';dbname=db_imi_test'
+        );
+    }
+    catch (\Throwable $th)
+    {
+        if('SQLSTATE[HY000] [2002] Connection refused' !== $th->getMessage())
+        {
+            throw $th;
+        }
+    }
+    sleep(1);
+}
