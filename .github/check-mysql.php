a1878aa8b83aa02ddae63c644cc54636093fe7b5
Yurun <admin@yurunsoft.com>
2021-11-12 17:31:53 +0800

    等待mysql启动完毕

--- .github/check-mysql.php
+++ .github/check-mysql.php
@@ -2,14 +2,16 @@
 
 declare(strict_types=1);
 
+require \dirname(__DIR__) . '/vendor/autoload.php';
+
 for ($i = 0; $i < 30; ++$i)
 {
     try
     {
         new PDO('mysql:'
-        . 'host=' . (getenv('MYSQL_SERVER_HOST') ?: '127.0.0.1')
-        . ';port=' . (getenv('MYSQL_SERVER_PORT') ?: 3306)
-        . ';dbname=db_imi_test', getenv('MYSQL_SERVER_USERNAME') ?: 'root', getenv('MYSQL_SERVER_PASSWORD') ?: 'root');
+        . 'host=' . imiGetEnv('MYSQL_SERVER_HOST', '127.0.0.1')
+        . ';port=' . imiGetEnv('MYSQL_SERVER_PORT', 3306)
+        . ';dbname=db_imi_test', imiGetEnv('MYSQL_SERVER_USERNAME', 'root'), imiGetEnv('MYSQL_SERVER_PASSWORD', 'root'));
 
         return;
     }
